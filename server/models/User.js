b06405602ce9d2230c999d937dd019268e83ec5d
Sayam Gautam <sayamgautam1@gmail.com>
2022-08-11 19:29:44 +0930

    Add resolver to add new skill for a user

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
-const { Schema, model, Types } = require('mongoose')
-const bcrypt = require('bcrypt')
+const { Schema, model, Types } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
   username: {
@@ -11,7 +11,7 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
+    match: [/.+@.+\..+/, "Must match an email address!"],
   },
   password: {
     type: String,
@@ -20,42 +20,42 @@ const userSchema = new Schema({
   },
   age: {
     type: String,
-    required: true,
+    // required: true,
     trim: true,
   },
   location: {
     type: String,
-    required: true,
+    // required: true,
     trim: true,
   },
   teachSkill: [
     {
       type: Schema.Types.ObjectId,
-      ref: 'Skill',
+      ref: "Skill",
     },
   ],
   learnSkill: [
     {
       type: Schema.Types.ObjectId,
-      ref: 'Skill',
+      ref: "Skill",
     },
   ], /// pernsonal details ,, array of courses. array of courses they can teach
   // If your user needs more properties, add them here. Don't forget to add them to the typeDefs.js, resolvers.js and the userSeeds.
-})
+});
 
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10
-    this.password = await bcrypt.hash(this.password, saltRounds)
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
   }
 
-  next()
-})
+  next();
+});
 
 userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password)
-}
+  return bcrypt.compare(password, this.password);
+};
 
-const User = model('User', userSchema)
+const User = model("User", userSchema);
 
-module.exports = User
+module.exports = User;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,49 +1,79 @@
-const { AuthenticationError } = require('apollo-server-express')
-const { User, Skill } = require('../models')
-const { signToken } = require('../utils/auth')
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Skill } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find()
+      return User.find();
     },
     skills: async () => {
-      return Skill.find()
+      return Skill.find().populate("students");
     },
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({
           email: context.user.email,
-        })
+        });
       }
-      throw new AuthenticationError('You must be signed in')
+      throw new AuthenticationError("You must be signed in");
     },
   },
 
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password })
-      const token = signToken(user)
-      return { token, user }
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
     login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email })
+      const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('No user found with this email address')
+        throw new AuthenticationError("No user found with this email address");
       }
 
-      const correctPw = await user.isCorrectPassword(password)
+      const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials')
+        throw new AuthenticationError("Incorrect credentials");
       }
 
-      const token = signToken(user)
+      const token = signToken(user);
 
-      return { token, user }
+      return { token, user };
+    },
+    addLearnSkill: async (parent, { learnSkill }, context) => {
+      console.log(context);
+      if (context.user) {
+        const skillExists = await Skill.findOne({ name: learnSkill });
+        console.log(skillExists);
+        if (skillExists) {
+          //check to skill exists // if not create it// after add id
+          return User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $push: { learnSkill: learnSkill._id } },
+            {
+              new: true,
+            }
+          );
+        } else {
+          const newSkill = await Skill.create({ name: learnSkill });
+          console.log(newSkill);
+          newSkill.students.push(context.user._id);
+          await newSkill.save();
+          return User.findByIdAndUpdate(
+            { _id: context.user._id },
+            { $push: { learnSkill: newSkill._id } },
+            {
+              new: true,
+            }
+          ).populate("learnSkill");
+        }
+      }
+      throw new AuthenticationError("Not logged in");
     },
   },
-}
+};
 
-module.exports = resolvers
+module.exports = resolvers;
