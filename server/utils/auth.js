f05fb95a706b21f412c3ed340adc717a555b0a06
Kym Reilly <kymreilly@hotmail.com>
2022-08-15 14:06:26 +0930

    extra seed filler, corrected env

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,32 +1,32 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
-const secret = process.env.JWT_SECRET;
-const expiration = '2h';
+const secret = `${process.env.JWT_SECRET_KEY}`
+const expiration = '2h'
 
 module.exports = {
   signToken: function ({ email, username, _id }) {
-    const payload = { email, username, _id };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    const payload = { email, username, _id }
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration })
   },
   authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization
 
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(' ').pop().trim()
     }
 
     if (!token) {
-      return req;
+      return req
     }
 
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      const { data } = jwt.verify(token, secret, { maxAge: expiration })
+      req.user = data
     } catch {
-      console.log('Invalid token');
+      console.log('Invalid token')
     }
 
-    return req;
+    return req
   },
-};
+}
