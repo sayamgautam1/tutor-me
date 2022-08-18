a3fcedbdebc0326273ac8a055ba85fa86378a304
Kym Reilly <kymreilly@hotmail.com>
2022-08-18 19:53:52 +0930

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
-const path = require('path');
-require('dotenv').config({path: path.join(__dirname, '../.env' )});
+const mongoose = require('mongoose')
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '../.env') })
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/final-project-test', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/final-project-test',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+git
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connection
